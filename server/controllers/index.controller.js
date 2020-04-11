66027a33a86556f2b2352a6c69742fb49b392188
vladislav <bincha.1997@gmail.com>
2020-04-11 22:36:28 +0900

    feat: added mail send controller

--- server/controllers/index.controller.js
+++ server/controllers/index.controller.js
@@ -1,20 +1,44 @@
 const db = require('../models/db');
+const config = require('config')
+const nodemailer = require('nodemailer')
 
-module.exports = {
-  sendMessage: function(req, res) {
-    try {
-      console.log('req :', req.body);
-      db.get('messages')
-        .push(req.body)
-        .write();
-      res.redirect('/');
-    } catch (e) {
-      res.status(500).json({ message: 'Что-то пошло не так, попробуйте снова...' });
+
+module.exports.sendMessage = (req, res) => {
+  try {
+    let { name, email, message } = req.body;
+    if (!name || !email || !message) {
+      req.flash('msgemail', 'Все поля нужно заполнить!');
+      res.redirect('/')
+      // return res.json({ msg: 'Все поля нужно заполнить!', status: 'Error' });
     }
-  },
-  getIndex: function(req, res) {
-    let skills = db.get('skills').value();
-    let products = db.get('products').value();
-    res.render('index', { title: 'Главная', skills, products});
-  },
+    db.get('messages').push(req.body).write();
+    const mailConfig = config.get('mail')
+    const transporter = nodemailer.createTransport(mailConfig.smtp);
+    const mailOptions = {
+      from: `"${name}" <${email}>`,
+      to: mailConfig.smtp.auth.user,
+      subject: mailConfig.subject,
+      text: message.trim().slice(0, 500) + `\n Отправлено с: <${email}>`,
+    };
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        return res.json({
+          msg: `При отправке письма произошла ошибка!: ${error}`,
+          status: 'Error',
+        });
+      }
+      // res.json({ msg: '', status: 'Ok' });
+    });
+    req.flash('msgemail', 'Письмо успешно отправлено!');
+    res.redirect('/');
+  } catch (e) {
+    res.status(500).json({ message: 'Что-то пошло не так, попробуйте снова...', error: e.message});
+  }
+};
+
+module.exports.getIndex = (req, res) => {
+  let msgemail = req.flash('msgemail')[0];
+  let skills = db.get('skills').value();
+  let products = db.get('products').value();
+  res.render('index', { title: 'Главная', skills, products, msgemail });
 };
--- server/controllers/login.controller.js
+++ server/controllers/login.controller.js
@@ -1,22 +1,21 @@
 const { isAuth } = require('../lib/auth');
 
-module.exports = {
-  loginUser: function (req, res) {
-    let { email, password } = req.body;
-    if (isAuth(email, password)) {
-      req.session.isAdmin = true;
-      res.redirect('/admin');
-    } else {
-      res.render('login', {
-        msglogin: 'Пользователь не найден',
-      });
-    }
-  },
-  getLogin: function (req, res) {
-    if (req.session.isAdmin) {
-      res.redirect('/admin');
-      return;
-    }
-    res.render('login', { title: 'Авторизация' });
-  },
+module.exports.loginUser = (req, res) => {
+  let { email, password } = req.body;
+  if (isAuth(email, password)) {
+    req.session.isAdmin = true;
+    res.redirect('/admin');
+  } else {
+    res.render('login', {
+      msglogin: 'Пользователь не найден',
+    });
+  }
+};
+
+module.exports.getLogin = (req, res) => {
+  if (req.session.isAdmin) {
+    res.redirect('/admin');
+    return;
+  }
+  res.render('login', { title: 'Авторизация' });
 };
